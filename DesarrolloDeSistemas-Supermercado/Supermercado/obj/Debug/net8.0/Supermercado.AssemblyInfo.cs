4339afd6e775ca1fc9737045e00effe49618644d
JosuGuzman <josu.guzmanet12d1@gmail.com>
2025-05-21 15:38:21 -0300

    detalles

--- DesarrolloDeSistemas-Supermercado/Supermercado/obj/Debug/net8.0/Supermercado.AssemblyInfo.cs
+++ DesarrolloDeSistemas-Supermercado/Supermercado/obj/Debug/net8.0/Supermercado.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermercado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a44ac74047e4fd692273a7ff951d40d622ca51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermercado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermercado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
